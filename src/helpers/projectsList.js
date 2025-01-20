63ad33e7b1690d1485b1463f240dacfa2efb5dc0
artemriabyi6 <ar04082001@gmail,com>
2025-01-20 17:28:24 +0200

    first version

---
+++ src/helpers/projectsList.js
@@ -0,0 +1,79 @@
+// import project01 from "./../img/projects/01.jpg";
+// import project01Big from "./../img/projects/01-big.jpg";
+
+import thalby from "./../img/projects/Thalby.png"
+import aperture from "./../img/projects/Aperture.png"
+import california from "./../img/projects/California.png"
+import minimal from "./../img/projects/Minimal.png"
+import healthcare from "./../img/projects/HealthCare.png"
+import john from "./../img/projects/John.png"
+
+
+
+
+
+
+// import project02 from "./../img/projects/02.jpg";
+// import project02Big from "./../img/projects/02-big.jpg";
+
+// import project03 from "./../img/projects/03.jpg";
+// import project03Big from "./../img/projects/03-big.jpg";
+
+// import project04 from "./../img/projects/04.jpg";
+// import project04Big from "./../img/projects/04-big.jpg";
+
+// import project05 from "./../img/projects/05.jpg";
+// import project05Big from "./../img/projects/05-big.jpg";
+
+// import project06 from "./../img/projects/06.jpg";
+// import project06Big from "./../img/projects/06-big.jpg";
+
+const projects = [
+	{
+		title: 'Thalby',
+		skills: 'React, Node.js, MongoDB',
+		img: thalby,
+		imgBig: thalby,
+		gitHubLink: 'https://github.com/artemriabyi6/thalby',
+	},
+	{
+		title: 'Aperture',
+		img: aperture,
+		imgBig: aperture,
+		skills: 'React, PHP, MySql',
+		gitHubLink: 'https://github.com/artemriabyi6/Aperture',
+	},
+	{
+		title: 'Minimal',
+		img: minimal,
+		imgBig: minimal,
+		skills: 'Vue JS, Node.js, MongoDB',
+		gitHubLink: 'https://github.com/artemriabyi6/Minimal-',
+	},
+	{
+		title: 'California',
+		img: california,
+		imgBig: california,
+		skills: 'React Native',
+		gitHubLink: 'https://github.com/artemriabyi6/California',
+
+	},
+	{
+		title: 'Healthcare',
+		img: healthcare,
+		imgBig: healthcare,
+		skills: 'HTML, SCSS, JS',
+		gitHubLink: 'https://github.com/artemriabyi6/Healthcare',
+
+	},
+	{
+		title: 'John',
+		img: john,
+		imgBig: john,
+		skills: 'React, PHP, MySql',
+		gitHubLink: 'https://github.com/artemriabyi6/John',
+
+	},
+];
+
+export {projects}
